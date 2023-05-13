6f671d524e0b9b3fbacc95d7d39f183e0ea1e4f8
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2023-05-13 19:21:11 +0200

    Back to working code

--- src/game/java/main/Game.java
+++ src/game/java/main/Game.java
@@ -4,8 +4,9 @@ public class Game implements Runnable{
     private GameWindow gameWindow;
     private GamePanel gamePanel;
     private Thread gameLoopThread;
+    private int frames = 0;
+    private long lastCheck = 0;
     private final int FPS = 120;
-    private final int tick = 200; //how many ticks per second
     public Game (){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
@@ -17,84 +18,27 @@ public class Game implements Runnable{
         gameLoopThread = new Thread(this);
         gameLoopThread.start();
     }
-/*private class runFrame extends Thread{ // **************** alternative version ****************
-        @Override
-        public void run(){
-            double timePerFrame = 1000000000.0 / FPS; // nanoseconds
-
-            long lastFrame = System.nanoTime();
-            long now = System.nanoTime();
-
-            int frames = 0;
-            double deltaFrame =0;
-            long lastCheck = System.currentTimeMillis();
-
-            while (true) {
-
-                now = System.nanoTime();
-                deltaFrame+=(now - lastFrame) / timePerFrame;
-                lastFrame=now;
-
-                if(deltaFrame>=1){
-                   gamePanel.repaint();
-                    frames++;
-                    deltaFrame--;
-                }
-
-                if (System.currentTimeMillis() - lastCheck >= 1000) {
-                    lastCheck = System.currentTimeMillis();
-                    System.out.println("FPS: " + frames);
-                    frames = 0;
-                }
-            }
-        }
-
-        }
-        */
-
-
 
     @Override
     public void run() {
         double timePerFrame = 1000000000.0 / FPS; // nanoseconds
-        double timePerTick = 1000000000.0 / tick;
-        long now = System.nanoTime();
-        long lastTick = System.nanoTime();
         long lastFrame = System.nanoTime();
-        int frames = 0;
-        int ticks = 0;
-
-        long lastCheck = System.currentTimeMillis();
-
-
-
-
-
+        long now = System.nanoTime();
+        frames = 0;
+        lastCheck = System.currentTimeMillis();
         while (true) {
-
             now = System.nanoTime();
-
-            if(now-lastFrame>=timePerFrame){
+            if (now - lastFrame >= timePerFrame) {
                 gamePanel.repaint();
-                lastFrame=now;
-                frames++;
+                lastFrame = now;
+                this.frames++;
             }
-            if(now - lastTick>= timePerTick){
-                // update()
-                lastTick=now;
-                ticks++;
-
-            }
-
 
             if (System.currentTimeMillis() - lastCheck >= 1000) {
-                lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames + " TICK: " + ticks);
-                frames = 0;
-                ticks=0;
+                this.lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                this.frames = 0;
             }
         }
-
-
     }
 }
