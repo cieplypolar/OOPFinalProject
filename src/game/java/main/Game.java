e4ee580d61be63656ca9a84760513db2b816e8ab
bartefix <bartek.procyk@student.uj.edu.pl>
2023-05-13 13:45:44 +0200

    push

--- src/game/java/main/Game.java
+++ src/game/java/main/Game.java
@@ -4,9 +4,8 @@ public class Game implements Runnable{
     private GameWindow gameWindow;
     private GamePanel gamePanel;
     private Thread gameLoopThread;
-    private int frames = 0;
-    private long lastCheck = 0;
     private final int FPS = 120;
+    private final int tick = 200; //how many ticks per second
     public Game (){
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
@@ -18,27 +17,111 @@ public class Game implements Runnable{
         gameLoopThread = new Thread(this);
         gameLoopThread.start();
     }
+/*private class runFrame extends Thread{ // **************** alternative version ****************
+        @Override
+        public void run(){
+            double timePerFrame = 1000000000.0 / FPS; // nanoseconds
+
+            long lastFrame = System.nanoTime();
+            long now = System.nanoTime();
+
+            int frames = 0;
+            double deltaFrame =0;
+            long lastCheck = System.currentTimeMillis();
+
+            while (true) {
+
+                now = System.nanoTime();
+                deltaFrame+=(now - lastFrame) / timePerFrame;
+                lastFrame=now;
+
+                if(deltaFrame>=1){
+                   gamePanel.repaint();
+                    frames++;
+                    deltaFrame--;
+                }
+
+                if (System.currentTimeMillis() - lastCheck >= 1000) {
+                    lastCheck = System.currentTimeMillis();
+                    System.out.println("FPS: " + frames);
+                    frames = 0;
+                }
+            }
+        }
+
+        }
+        */
+private class runFrame extends Thread{
+    @Override
+    public void run(){
+        double timePerFrame = 1000000000.0 / FPS; // nanoseconds
+
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        while (true) {
+
+            now = System.nanoTime();
+
+           if(now-lastFrame>=timePerFrame){
+            gamePanel.repaint();
+            lastFrame=now;
+            frames++;
+           }
+
+
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+    }
+
+}
+private class runTick extends Thread{
+        @Override
+        public void run() {
+            double timePerTick = 1000000000.0 / tick;
+            long now = System.nanoTime();
+            long lastTick = System.nanoTime();
+            int ticks = 0;
+            double deltaTick =0;
+            long lastCheck = System.currentTimeMillis();
+
+            while(true){
+
+                now = System.nanoTime();
+                deltaTick+=(now - lastTick) / timePerTick;
+                lastTick=now;
+                if(deltaTick>=1){
+                    // update() TODO
+                     ticks++;
+                     deltaTick--;
+                }
+
+                if (System.currentTimeMillis() - lastCheck >= 1000) {
+                    lastCheck = System.currentTimeMillis();
+                    System.out.println("TICK: " + ticks);
+                    ticks = 0;
+                }
+
+            }
+
+        }
+
+
+    }
 
     @Override
     public void run() {
-        double timePerFrame = 1000000000.0 / FPS; // nanoseconds
-        long lastFrame = System.nanoTime();
-        long now = System.nanoTime();
-        frames = 0;
-        lastCheck = System.currentTimeMillis();
-        while (true) {
-            now = System.nanoTime();
-            if (now - lastFrame >= timePerFrame) {
-                gamePanel.repaint();
-                lastFrame = now;
-                this.frames++;
-            }
-
-            if (System.currentTimeMillis() - lastCheck >= 1000) {
-                this.lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames);
-                this.frames = 0;
-            }
-        }
+        Thread f = new runFrame();
+        Thread t = new runTick();
+        f.start();
+        t.start();
     }
 }
