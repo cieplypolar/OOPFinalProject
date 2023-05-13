c721176470b9629883411298df3101e88d8d2de6
bartefix <bartek.procyk@student.uj.edu.pl>
2023-05-13 19:25:33 +0200

    Revert "push"
    This reverts commit 05a75a5130d7625cf77d1522c88375851bd14611.

--- src/game/java/main/Game.java
+++ src/game/java/main/Game.java
@@ -51,50 +51,77 @@ public class Game implements Runnable{
 
         }
         */
-
-
-
+private class runFrame extends Thread{
     @Override
-    public void run() {
+    public void run(){
         double timePerFrame = 1000000000.0 / FPS; // nanoseconds
-        double timePerTick = 1000000000.0 / tick;
-        long now = System.nanoTime();
-        long lastTick = System.nanoTime();
+
         long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+
         int frames = 0;
-        int ticks = 0;
-
         long lastCheck = System.currentTimeMillis();
-
-
-
-
 
         while (true) {
 
             now = System.nanoTime();
 
-            if(now-lastFrame>=timePerFrame){
-                gamePanel.repaint();
-                lastFrame=now;
-                frames++;
-            }
-            if(now - lastTick>= timePerTick){
-                // update()
-                lastTick=now;
-                ticks++;
+           if(now-lastFrame>=timePerFrame){
+            gamePanel.repaint();
+            lastFrame=now;
+            frames++;
+           }
 
-            }
 
 
             if (System.currentTimeMillis() - lastCheck >= 1000) {
                 lastCheck = System.currentTimeMillis();
-                System.out.println("FPS: " + frames + " TICK: " + ticks);
+                System.out.println("FPS: " + frames);
                 frames = 0;
-                ticks=0;
             }
+        }
+    }
+
+}
+private class runTick extends Thread{
+        @Override
+        public void run() {
+            double timePerTick = 1000000000.0 / tick;
+            long now = System.nanoTime();
+            long lastTick = System.nanoTime();
+            int ticks = 0;
+            double deltaTick =0;
+            long lastCheck = System.currentTimeMillis();
+
+            while(true){
+
+                now = System.nanoTime();
+                deltaTick+=(now - lastTick) / timePerTick;
+                lastTick=now;
+                if(deltaTick>=1){
+                    // update() TODO
+                     ticks++;
+                     deltaTick--;
+                }
+
+                if (System.currentTimeMillis() - lastCheck >= 1000) {
+                    lastCheck = System.currentTimeMillis();
+                    System.out.println("TICK: " + ticks);
+                    ticks = 0;
+                }
+
+            }
+
         }
 
 
+    }
+
+    @Override
+    public void run() {
+        Thread f = new runFrame();
+        Thread t = new runTick();
+        f.start();
+        t.start();
     }
 }
